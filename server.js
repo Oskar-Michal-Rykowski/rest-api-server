e1db7cdebb32ebab2d2eb31b17482cadea75d883
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-12-05 20:35:42 +0100

    Add frontend part

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 //import routes
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const seatsRoutes = require('./routes/seats.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.listen(8000, () => {
