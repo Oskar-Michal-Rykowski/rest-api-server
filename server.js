1289e59885bc0858e4059bda0a4e87b8873e1a3d
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-12-09 21:38:31 +0100

    Ready do publish on heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const path = require('path');
+
+const app = express();
 
 //import routes
 const testimonialsRoutes = require('./routes/testimonials.routes');
@@ -10,7 +12,7 @@ const concertsRoutes = require('./routes/concerts.routes');
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
 
@@ -18,6 +20,14 @@ app.listen(8000, () => {
 app.use('/api', testimonialsRoutes);
 app.use('/api', seatsRoutes);
 app.use('/api', concertsRoutes);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+//show our react app in every link the server didn't match with any endpoint
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 //other
 app.use((req, res) => {
